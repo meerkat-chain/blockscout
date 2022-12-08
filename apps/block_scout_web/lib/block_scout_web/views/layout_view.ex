57fa3e2c8c043360addcb6d658326d0188ba681e
Path <119656644+pathpompeii@users.noreply.github.com>
2022-12-08 13:34:10 +0800

    feat: new UI skin

--- apps/block_scout_web/lib/block_scout_web/views/layout_view.ex
+++ apps/block_scout_web/lib/block_scout_web/views/layout_view.ex
@@ -9,28 +9,10 @@ defmodule BlockScoutWeb.LayoutView do
 
   @default_other_networks [
     %{
-      title: "POA",
-      url: "https://blockscout.com/poa/core"
-    },
-    %{
-      title: "Sokol",
-      url: "https://blockscout.com/poa/sokol",
+      title: "Mchain Testnet",
+      url: "https://testnet-explorer.mchain.dev",
       test_net?: true
     },
-    %{
-      title: "Gnosis Chain",
-      url: "https://blockscout.com/xdai/mainnet"
-    },
-    %{
-      title: "Ethereum Classic",
-      url: "https://blockscout.com/etc/mainnet",
-      other?: true
-    },
-    %{
-      title: "RSK",
-      url: "https://blockscout.com/rsk/mainnet",
-      other?: true
-    }
   ]
 
   alias BlockScoutWeb.SocialMedia
